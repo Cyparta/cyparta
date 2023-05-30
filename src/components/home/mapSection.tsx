f58118298145b5911ce09c0c5a877302f7836ae6
hosam8081 <hosamsalah8081@gmail.com>
2023-05-30 10:21:10 +0300

    edit error

--- src/components/home/mapSection.tsx
+++ src/components/home/mapSection.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { VectorMap } from "@south-paw/react-vector-maps";
+import { VectorMap, VectorMapProps } from "@south-paw/react-vector-maps";
 import { Box, Typography } from "@mui/material";
 import webIcon from "../../assets/home/web-icon.png";
 import appIcon from "../../assets/home/app-icon.png";
 import dashIcon from "../../assets/home/dash-icon.png";
 
-
 // You'll need to download the json file from the docs site or you can create your own.
 import * as world from "./world.json";
 import Image from "next/image";
+
+
+interface MapContainerProps {
+    selectedCountry: string[];
+}
 
 const MapContainer = styled.div`
   margin: 1rem auto;
@@ -39,14 +43,14 @@ const MapContainer = styled.div`
 
       // When a layer is 'selected' (via currentLayers prop).
       &[aria-current="true"] {
-        fill: ${({ selectedCountry }) => {
+        fill: ${({ selectedCountry }: MapContainerProps) => {
+
         if (JSON.stringify(selectedCountry) == JSON.stringify(["us", "eg"])) {
             return "green"; // Make Egypt green when Website is selected
-        } else if (selectedCountry === "eg") {
-            return "blue"; // Make Egypt blue when Dashboards is selected
         } else {
             return "rgba(15, 143, 151, 1)";
         }
+
     }};
       }
     }
@@ -56,18 +60,19 @@ const MapContainer = styled.div`
 const Map = () => {
     const [selectedCountry, setSelectedCountry] = useState(["us"]);
 
-    const handleSelection = (countryCode) => {
+    const handleSelection = (countryCode: string[]): void => {
         setSelectedCountry(countryCode);
     };
 
     return (
         <>
-                <Typography sx={{textAlign:"center", fontSize:"24px", fontWeight:"500", mt:"48px"}}>GLOBAL PRESENCE</Typography>
-            <MapContainer>
+            <Typography sx={{ textAlign: "center", fontSize: "24px", fontWeight: "500", mt: "48px" }}>GLOBAL PRESENCE</Typography>
+            <MapContainer selectedCountry={[]}>
                 <VectorMap
-                    {...world}
+                    {...(world as VectorMapProps)}
                     checkedLayers={selectedCountry}
-                    onLayerClick={handleSelection}
+                    // onLayerClick={handleSelection}
+                    
                 />
             </MapContainer>
 
@@ -75,7 +80,7 @@ const Map = () => {
                 sx={{ color: "rgba(223, 223, 223, 1)", width: "100%", height: "1px" }}
             ></Box>
 
-            <Box sx={{ display: "flex", justifyContent: "center", gap: "24px", mb:"74px", flexWrap:"wrap" }}>
+            <Box sx={{ display: "flex", justifyContent: "center", gap: "24px", mb: "74px", flexWrap: "wrap" }}>
                 <Box
                     sx={{ cursor: "pointer", display: "flex", gap: "12px" }}
                     onClick={() => handleSelection(["us", "eg"])}
@@ -100,7 +105,7 @@ const Map = () => {
                     </Typography>
                 </Box>
                 <Box
-                    sx={{ cursor: "pointer", display:"flex", gap:"12px" }}
+                    sx={{ cursor: "pointer", display: "flex", gap: "12px" }}
                     onClick={() => handleSelection(["eg", "us", "ca"])}
                 >
                     <Image src={dashIcon} alt="applications" width="26" height="21" />
