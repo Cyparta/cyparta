b139a4fd65dfcf4329e00f0f1f49461594abf6a4
hosam8081 <hosamsalah8081@gmail.com>
2023-07-04 15:22:49 +0300

    push with export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
+    exportTrailingSlash: true,
     output: 'export',
     distDir:"_static",
     // webpack5: false,
