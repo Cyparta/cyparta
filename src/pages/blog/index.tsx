9dfc0725d94a5776debb8175e38cc1a0e8ed82df
hosam8081 <hosamsalah8081@gmail.com>
2023-07-05 09:53:09 +0300

    upload with test page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <Link href="/blog/1">
+            test
+        </Link>
+    </div>
+  )
+}
+
+export default Page
